fb563db4af1880f43c9b6a508959cfa7aa4ab66f
Shutooo46 <kususyuto@gmail.com>
2026-05-29 19:49:26 +0900

    fix: html要素にsuppressHydrationWarningを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja" className={`${pressStart.variable} ${dmSans.variable}`}>
+    <html lang="ja" className={`${pressStart.variable} ${dmSans.variable}`} suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: `(function(){if(localStorage.getItem('theme')==='dark')document.documentElement.classList.add('dark');})()` }} />
       </head>
